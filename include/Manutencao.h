2d2ff38f0d5b0c8b8d4efe9c802305d2df4d1194
Matheus Bispo <matheus.mbispo@gmail.com>
2016-10-25 16:07:09 -0200

    Arquivos de Cabeçalho do projeto!

---
+++ include/Manutencao.h
@@ -0,0 +1,44 @@
+/********************************************//**
+ * @file Manutencao.h
+ * @brief Contem os cabeçalhos de todas as
+ *        funcoes de operações com Manutenção, e
+ *        a estrutura do tipo Manutenção e do tipo Data.
+ * @bug Nao contem bugs conhecidos!
+ *
+ ***********************************************/
+#ifndef MANUNTENCAO_H_SECURE
+#define MANUNTENCAO_H_SECURE
+
+#include "Win_FuncoesBasicas.h"
+#include "Erros.h"
+#include <time.h>
+
+/********************************************//**
+ * \brief Contem informações sobre uma data
+ ***********************************************/
+typedef struct data{
+	int dia, mes, ano;
+}Data;
+
+/********************************************//**
+ * \brief Contem informações sobre uma manutenção
+ ***********************************************/
+typedef struct manutencao{
+	char placa[TAM_PLACA];
+	char cpf[TAM_CPF];
+	char descricao[TAM_DESCRICAO];
+	float valorPecas;
+	float valorObra;
+	Data data;
+}Manutencao;
+
+//----------------------------------------------------------------------//
+//Manutencao
+int incluiManutencao(Manutencao m);
+int excluiManutencao(char *placa);
+int buscaManutencao(char *placa, char *cpf, Data data, int *pos);
+void obtemDataAtual(Data *data);
+int comparaData(Data data1, Data data2);
+Data convertTime(SYSTEMTIME st);
+//----------------------------------------------------------------------//
+#endif
---
+++ include/Proprietario.h
@@ -0,0 +1,50 @@
+/********************************************//**
+ * @file Proprietario.h
+ * @brief Contem os cabeçalhos de todas as
+ *        funcoes de operações com Proprietario, e
+ *        a estrutura do tipo Proprietario e do tipo Endereço.
+ * @bug Nao contem bugs conhecidos!
+ *
+ ***********************************************/
+
+#ifndef PROPRIETARIO_H_SECURE
+#define PROPRIETARIO_H_SECURE
+
+#include "Win_FuncoesBasicas.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/********************************************//**
+ * \brief Guarda um endereço
+ ***********************************************/
+typedef struct endereco{
+	char descricao[TAM_DESCRICAO];/**< A descrição do endereço*/
+	char cidade[TAM_CIDADE];/**< A cidade do endereço */
+	char estado[TAM_ESTADO];/**< O estado do endereço */
+}Endereco;
+
+
+/********************************************//**
+ * \brief Guarda as informações de
+ *        um proprietário
+ ***********************************************/
+typedef struct proprietario{
+	char nome[TAM_NOME];/**< Nome do proprietario */
+	char cpf[TAM_CPF];/**< CPF do proprietario */
+	Endereco endereco;/**< Endereço do proprietario */
+	char telefone[TAM_TEL];/**< Telefone do proprietario */
+}Proprietario;
+
+//----------------------------------------------------------------------//
+//Proprietario
+int incluiProprietario(Proprietario p);
+int buscaProprietario(char *cpf, int *pos);
+int alteraProprietario(Proprietario novoP, char *cpf);
+int excluiProprietario(char *cpf);
+Proprietario *pesquisaProprietario(char *nome);
+int pegaProprietario(char *cpf, Proprietario *pAux);
+int validaCPF(char *cpf);
+int validaTelefone(char *tel);
+
+//----------------------------------------------------------------------//
+#endif
